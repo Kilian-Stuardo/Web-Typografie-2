228a39409851b7b4099c341145644e33c03596e7
Kilian-Stuardo <45419401+Kilian-Stuardo@users.noreply.github.com>
2020-05-20 20:14:47 +0200

    .

---
+++ sounds.js
@@ -0,0 +1,39 @@
+// Hier kan je tijden toevoegen in secondes
+// Op die mommenten komt er een class op de body.
+sounds = [
+
+0, //sound0
+
+9, //sound1
+
+15, //sound2
+
+16.5, //sound3  baseline beep
+
+23, //sound4 fuck off
+
+25, //sound5 baseline beep
+
+31, //sound6 baseline beep
+
+33, //sound7 begin test
+
+39,//sound8 tension building
+
+52, //sound9
+
+58,//sound10
+
+77, //sound11
+
+86, // sound12 frame between scenes
+
+88, // sound 13 begin scene 2
+
+103, // sound 14 inspection
+
+119, // sound 15 basline scanner
+
+130 // sound 16 not close to baseline
+
+];
